4b4ca10b85f3c85f25736bd3cee1e71216b1017b
JenkaCool <Cool.diatel@yandex.ru>
2023-10-15 10:57:47 +0300

    update results page

--- fronend/src/components/ResultsPage.js
+++ fronend/src/components/ResultsPage.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import {Route, Routes, BrowserRouter, Link} from 'react-router-dom';
 import { useNavigate, Outlet } from "react-router-dom";
 
+import '../styles/ResultsPage.css';
+
 import Button from './Button';
 
 function ResultsPage() {
@@ -10,9 +12,15 @@ function ResultsPage() {
         <>
             <Button text={"Вернуться"} link={`/`}/>
             <div className="results-list">
-                <div className="analysis"></div>
-                <div className="compare"></div>
-                <div className="recomend"></div>
+                <div className="analysis">
+                    <h3>Статистика</h3>
+                </div>
+                <div className="compare">
+                    <h3>Сравнение с другими районами</h3>
+                </div>
+                <div className="recomend">
+                    <h3>Рекомендации</h3>
+                </div>
             </div>
         </>
     );
